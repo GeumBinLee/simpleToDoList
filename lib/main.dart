1204a0ea4699d882a7b1e0bf95e585dbc9a497a5
GeumBin <lgb9811@gmail.com>
2023-10-14 17:01:28 +0900

    Feat: Provider 패키지  활용

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_list/bucket_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => BucketService())],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -26,149 +33,100 @@ class Bucket {
 }
 
 /// 홈 페이지
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<Bucket> bucketList = [];
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
-        title: Text(
-          "TO DO LIST",
-          style: TextStyle(color: Colors.black),
+    return Consumer<BucketService>(builder: (context, bucketService, child) {
+      // BucketService의 bucketList 가져오기
+      List<Bucket> bucketList = bucketService.bucketList;
+      return Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
+          title: Text(
+            "TO DO LIST",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.pink[100],
         ),
-        backgroundColor: Colors.pink[100],
-      ),
-      body: bucketList.isEmpty
-          ? Center(child: Text("할 일을 작성해 주세요."))
-          : ListView.builder(
-              itemCount: bucketList.length, // bucketList 개수 만큼 보여주기
-              itemBuilder: (context, index) {
-                Bucket bucket = bucketList[index]; // index에 해당하는 bucket 가져오기
-
-                return ListTile(
-                  // 버킷 리스트 할 일
-                  title: Text(
-                    bucket.job,
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: bucket.isDone ? Colors.grey : Colors.black,
-                    ),
-                  ),
-                  leading: IconButton(
-                    icon: bucket.isDone
-                        ? Icon(CupertinoIcons.checkmark_alt_circle_fill)
-                        : Icon(CupertinoIcons.checkmark_alt_circle),
-                    color: bucket.isDone ? Colors.red : Colors.black,
-                    onPressed: () {
-                      //체크 버튼 클릭 시
-                      print('$bucket : 체크하기');
-                    },
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      // 수정 아이콘 버튼
-                      IconButton(
-                        icon: Icon(
-                          CupertinoIcons.pencil,
-                          color: Colors.black,
+        body: bucketList.isEmpty
+            ? Center(child: Text("할 일을 작성해 주세요."))
+            : ListView.builder(
+                itemCount: bucketList.length, // bucketList 개수 만큼 보여주기
+                itemBuilder: (context, index) {
+                  var bucket = bucketList[index]; // index에 해당하는 bucket 가져오기
+                  return ListTile(
+                      // 버킷 리스트 할 일
+                      title: Text(
+                        bucket.job,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: bucket.isDone ? Colors.grey : Colors.black,
                         ),
+                      ),
+                      leading: IconButton(
+                        icon: bucket.isDone
+                            ? Icon(CupertinoIcons.checkmark_alt_circle_fill)
+                            : Icon(CupertinoIcons.checkmark_alt_circle),
+                        color: bucket.isDone ? Colors.red : Colors.black,
                         onPressed: () {
-                          // 수정 버튼 클릭시
+                          //체크 버튼 클릭 시
+                          bucket.isDone = !bucket.isDone;
+                          bucketService.updateBucket(bucket, index);
+                          print('$bucket : 체크하기');
                         },
                       ),
                       // 삭제 아이콘 버튼
-                      IconButton(
-                        icon: Icon(
-                          CupertinoIcons.delete,
-                          color: Colors.black,
-                        ),
-                        onPressed: () {
-                          // 삭제 버튼 클릭시
-                          showDeleteDialog(context, index);
-                        },
-                      )
-                    ],
-                  ),
-                  onTap: () {
-                    // 아이템 클릭 시
-                    setState(() {
-                      bucket.isDone = !bucket.isDone;
-                    });
-                  },
-                );
-              },
-            ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.pink[100],
-        child: Icon(
-          Icons.add,
-          color: Colors.black,
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // 수정 아이콘 버튼
+                          IconButton(
+                            icon: Icon(
+                              CupertinoIcons.pencil,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {
+                              // 수정 버튼 클릭시
+                            },
+                          ),
+                          // 삭제 아이콘 버튼
+                          IconButton(
+                            icon: Icon(
+                              CupertinoIcons.delete,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {
+                              // 삭제 버튼 클릭시
+                              bucketService.deleteBucket(index);
+                            },
+                          )
+                        ],
+                      ),
+                      onTap: () {
+                        // 아이템 클릭 시
+                      });
+                },
+              ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.pink[100],
+          child: Icon(
+            Icons.add,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            // + 버튼 클릭시 버킷 생성 페이지로 이동
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => CreatePage()),
+            );
+          },
         ),
-        onPressed: () async {
-          // + 버튼 클릭시 버킷 생성 페이지로 이동
-          String? job = await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (_) => CreatePage()),
-          );
-          if (job != null) {
-            setState(() {
-              // bucketList.add(job); // 원래 코드
-              Bucket newBucket = Bucket(job, false);
-              bucketList.add(newBucket); // 버킷 리스트에 추가
-            });
-          }
-        },
-      ),
-    );
+      );
+    });
   }
-
-  void showDeleteDialog(BuildContext context, int index) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("정말로 삭제하시겠습니까?"),
-          actions: [
-            // 취소 버튼
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text(
-                "취소",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            // 확인 버튼
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  // index에 해당하는 항목 삭제
-                  bucketList.removeAt(index);
-                });
-                Navigator.pop(context);
-              },
-              child: Text(
-                "확인",
-                style: TextStyle(color: Colors.pink),
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
+} // <-- Missing closing bracket for HomePage
 
 /// 버킷 생성 페이지
 class CreatePage extends StatefulWidget {
@@ -209,20 +167,24 @@ class _CreatePageState extends State<CreatePage> {
             // 텍스트 입력창
             TextField(
               onSubmitted: (_) {
-                String job =
-                    textController.text; // Get the value from the TextField
+                // 텍스트 입력 시
+
+                String job = textController.text; // 값 가져오기
                 if (job.isEmpty) {
                   setState(() {
-                    error =
-                        "내용을 입력해주세요."; // Show an error message if the TextField is empty
+                    error = "내용을 입력해주세요."; // 내용이 없는 경우 에러 메세지
                   });
                 } else {
                   setState(() {
-                    error = null; // Clear the error message
+                    error = null; // 내용이 있는 경우 에러 메세지 숨기기
                   });
-                  Navigator.pop(context,
-                      job); // Close the CreatePage and return the entered value
+                  //BucketService 가져오기
+                  BucketService bucketService = context.read<BucketService>();
+                  bucketService.createBucket(job);
+
+                  Navigator.pop(context, job); // job 변수를 반환하며 화면을 종료합니다.
                 }
+                print(job);
               },
               controller: textController,
               autofocus: true,
@@ -263,6 +225,10 @@ class _CreatePageState extends State<CreatePage> {
                     setState(() {
                       error = null; // 내용이 있는 경우 에러 메세지 숨기기
                     });
+                    //BucketService 가져오기
+                    BucketService bucketService = context.read<BucketService>();
+                    bucketService.createBucket(job);
+
                     Navigator.pop(context, job); // job 변수를 반환하며 화면을 종료합니다.
                   }
                   print(job);
